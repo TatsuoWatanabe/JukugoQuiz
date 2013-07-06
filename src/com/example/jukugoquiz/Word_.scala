e44f8ccd4f779292b94aad238ed523d55c7f027c
tatsuo <tatsuo1234@gamma.ocn.ne.jp>
2013-07-06 09:22:05 +0900

    commit

--- src/com/example/jukugoquiz/Word_.scala
+++ src/com/example/jukugoquiz/Word_.scala
@@ -4,9 +4,11 @@ case class Word_(kanji: String, yomi: String) {
   lazy val Char1 = kanji.charAt(0);
   lazy val Char2 = kanji.charAt(1);
   def Search(target: Char): SearchResult = {
-    if(target == this.Char1){ SearchResult.Post }
-    else if(target == Char2){ SearchResult.Pre }
-    else                    { SearchResult.NotFound }
+    target match {
+      case this.Char1 => SearchResult.Post
+      case this.Char2 => SearchResult.Pre
+      case _          => SearchResult.NotFound
+    }
   }
 }
 
