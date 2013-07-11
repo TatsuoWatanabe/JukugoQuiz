e8b368dad4de3ed0705ae60cada12a6ab7994ffe
tatsuo1234@gamma.ocn.ne.jp <tatsuo1234@gamma.ocn.ne.jp>
2013-07-11 15:21:41 +0900

    commit

--- src/com/example/jukugoquiz/MainActivity.scala
+++ src/com/example/jukugoquiz/MainActivity.scala
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.app.Activity
 import android.view.Menu
 import android.widget._
+import java.io.IOException
 
 class MainActivity extends Activity {
 
   override protected def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    this.initialize
   }
 
   override def onCreateOptionsMenu(menu: Menu): Boolean = {
@@ -26,11 +28,68 @@ class MainActivity extends Activity {
     this.showMessage(msg)
   }
   */
+  
+  private def initialize() {
+    val tb = findViewById(R.id.table).asInstanceOf[TableLayout]
+    val et = findViewById(R.id.editTextAnswer).asInstanceOf[EditText]
+    try {
+      loadData()
+    } catch { case e: IOException =>
+      showMessage(e.getLocalizedMessage)
+    }
+    
+    showQuestion()
+  }
+  
+  private def loadData() {
+    import java.io._
+    showMessage("loadData")
+    try{
+      val ips: InputStream = getResources().getAssets().open("Jukugo2c_utf8.txt")
+      val br = new BufferedReader(
+        new InputStreamReader(ips)   
+      )
+    }
+    val as = getResources().getAssets()
+  }
+  
+  private def showQuestion() {
+    showMessage("showQuestion")
+  }
+  
+  /*
+  private void initialize() {
+    try {
+      LoadData();
+    } catch (IOException e) {
+      showMessage(e.getLocalizedMessage());
+      return;
+    }
+    showQuestion();
+  }
+   */
+  /*
+ private void LoadData() throws IOException {
+    AssetManager as = getResources().getAssets();
+    try {
+      InputStream is = as.open("Jukugo2c_utf8.txt");
+      BufferedReader br = new BufferedReader(
+        new InputStreamReader(is));
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] columns = line.split(",");
+        Word w = new Word(columns[0], columns[1]);
+        Dictionary.add(w);
+      }
+    } catch (IOException e) {
+      throw new IOException(e.toString());
+    }
+  }
+   */
 
   def showMessage(s: String){
     val b = new android.app.AlertDialog.Builder(this)
     b.setMessage(s).setPositiveButton(android.R.string.ok, null).show()
-    android.util.Log.d(getString(R.string.app_name), s)
   }
 
 
