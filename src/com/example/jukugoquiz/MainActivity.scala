2ca7fea8ab0fab07e22b5025fb11d99954068c6d
tatsuo1234@gamma.ocn.ne.jp <tatsuo1234@gamma.ocn.ne.jp>
2013-07-14 15:44:43 +0900

    commit

--- src/com/example/jukugoquiz/MainActivity.scala
+++ src/com/example/jukugoquiz/MainActivity.scala
@@ -5,9 +5,13 @@ import android.app.Activity
 import android.view.Menu
 import android.widget._
 import java.io.IOException
+import java.util.Date
+import scala.util.Random
 
 class MainActivity extends Activity {
-  var wordList: List[Word]
+  var wordList: Array[Word] = Array()
+  var Answers: scala.collection.mutable.ArrayBuffer[Word] = scala.collection.mutable.ArrayBuffer.fill(8)(null)
+  var AnswerChar: Char = 0
 
   override protected def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
@@ -29,127 +33,99 @@ class MainActivity extends Activity {
     this.showMessage(msg)
   }
   */
-
+  case class Watch (label: String) {
+    private val start: Date = new Date 
+    def log() = {
+      android.util.Log.d(label, "Watch -- " + label + " : " + ((new Date).getTime - start.getTime).toString + " ms")
+    }
+  }
+  /**
+   * initialize
+   */
   private def initialize() {
     val tb = findViewById(R.id.table).asInstanceOf[TableLayout]
     val et = findViewById(R.id.editTextAnswer).asInstanceOf[EditText]
     try {
+      val watch = Watch("loadData")
       loadData()
+      watch.log
+      
     } catch { case e: IOException =>
       showMessage(e.getLocalizedMessage)
     }
 
+    val watch = Watch("showQuestion")
     showQuestion()
+    watch.log
+    
   }
 
+  /**
+   * loadData
+   */
   private def loadData() {
     import java.io._
-    showMessage("loadData")
     try{
       val br = new BufferedReader(
         new InputStreamReader(getResources.getAssets.open("Jukugo2c_utf8.txt"))
       )
-      val lines = Iterator.continually(br.readLine).takeWhile(_ != null).toList
+      val lines = Iterator.continually(br.readLine).takeWhile(_ != null).toArray
       this.wordList = lines.map(_.split(",")).map(arr => new Word(arr(0), arr(1)))
     } catch { case e: IOException =>
       throw new IOException(e.getMessage.toString)
     }
   }
 
+  /**
+   * showQuestion
+   */
   private def showQuestion() {
+    import scala.collection.mutable.ArrayBuffer
+    import scala.util.Random
+    
     val et = findViewById(R.id.editTextAnswer).asInstanceOf[EditText]
     et.setText("")
-    val candidateCount = 0
-    val selectedWord = ""
-    val selectedChar: Char = 'a'
-    val preCandidate: Array[Word] = Array()
-    val postCandidate: Array[Word] = Array()
-    val rnd = new java.util.Random()
-    showMessage("showQuestion")
-    
-    this.wordList
-    
-  }
+    var candidateCount = 0
+    val preCandidate: ArrayBuffer[Word] = ArrayBuffer()
+    val postCandidate: ArrayBuffer[Word] = ArrayBuffer()
 
-  /*
-  private void showQuestion() {
-    EditText et = (EditText) findViewById(
-      R.id.editTextAnswer);
-    et.setText("");
-    int candidateCount = 0;
-    String selectedWord = "";
-    char selectedChar = 0;
-    ArrayList<Word> preCandidate = 
-      new ArrayList<Word>();
-    ArrayList<Word> postCandidate = 
-      new ArrayList<Word>();
-    Random rnd = new Random();
-
-    while (candidateCount < 4) {
-      selectedWord = Dictionary.get(
-        rnd.nextInt(Dictionary.size())).Kanji;
-      selectedChar = selectedWord.charAt(
-        rnd.nextInt(2));
-      preCandidate.clear();
-      postCandidate.clear();
-      for (Word w : Dictionary) {
-        SearchResult sr = w.Search(selectedChar);
-        switch (sr) {
-        case Pre:
-          preCandidate.add(w);
-          break;
-        case Post:
-          postCandidate.add(w);
-          break;
-        default:
+    while((preCandidate.size + postCandidate.size) < 4){
+      val selectedWord = this.wordList((new Random).nextInt(this.wordList.size)).kanji
+      val selectedChar = selectedWord.charAt((new Random).nextInt(2))
+      
+      preCandidate.clear
+      postCandidate.clear
+      AnswerChar = selectedChar
+      
+      for(w <- this.wordList){
+        w.search(selectedChar) match {
+          case SearchResult.Pre => preCandidate += w
+          case SearchResult.Post => postCandidate += w
+          case _ =>
         }
       }
-      candidateCount = preCandidate.size() + 
-        postCandidate.size();
+    }// end while
+    
+    this.showMessage(AnswerChar.toString)
+    
+    Answers = ArrayBuffer.fill(8)(Word("●●", ""))
+    //preCandidateからランダムに4件取り出してAnswers配列の前半に格納
+    Random.shuffle(preCandidate).take(4).zipWithIndex.foreach{ case (w:Word , i: Int) =>
+      Answers(i) = w
+      val vId = getResources.getIdentifier("textView" + (i + 1).toString, "id", getPackageName())
+      findViewById(vId).asInstanceOf[TextView].setText(w.char1.toString)
     }
-
-    for (int i = 0; i < 8; i++)
-      Answers[i] = null;
-
-    if (preCandidate.size() > 4) {
-      for (int i = 0; i < 4; i++) {
-        int index = rnd.nextInt(
-          preCandidate.size());
-        Answers[i] = preCandidate.get(index);
-        preCandidate.remove(index);
-      }
-    } else {
-      for (int i = 0; i < preCandidate.size(); i++)
-        Answers[i] = preCandidate.get(i);
+    //postCandidateからランダムに4件取り出してAnswers配列の後半に格納
+    Random.shuffle(postCandidate).take(4).zipWithIndex.foreach{ case (w:Word , i: Int) =>
+      Answers(i + 4) = w
+      val vId = getResources.getIdentifier("textView" + (i + 5).toString, "id", getPackageName())
+      findViewById(vId).asInstanceOf[TextView].setText(w.char2.toString)
     }
-
-    if (postCandidate.size() > 4) {
-      for (int i = 0; i < 4; i++) {
-        int index = rnd.nextInt(
-          postCandidate.size());
-        Answers[i + 4] = postCandidate.get(index);
-        postCandidate.remove(index);
-      }
-    } else {
-      for (int i = 0; i < postCandidate.size(); i++)
-        Answers[i + 4] = postCandidate.get(i);
-    }
-
-    for (int i = 0; i < 8; i++) {
-      if (Answers[i] == null)
-        TextViews[i].setText("●");
-      else {
-        int startIndex = 1;
-        if (i < 4)
-          startIndex = 0;
-        TextViews[i].setText(Answers[i].Kanji.
-          substring(startIndex, startIndex + 1));
-      }
-    }
-    AnswerChar = selectedChar;
   }
-   */
 
+  /**
+   * showMessage
+   */
   def showMessage(s: String){
     val b = new android.app.AlertDialog.Builder(this)
     b.setMessage(s).setPositiveButton(android.R.string.ok, null).show()
