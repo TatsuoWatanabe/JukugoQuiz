1d971ca01a8d378bb41ebe1364e8b1df1bc4f78b
tatsuo1234@gamma.ocn.ne.jp <tatsuo1234@gamma.ocn.ne.jp>
2013-07-17 00:36:56 +0900

    scala化一旦完了

--- src/com/example/jukugoquiz/MainActivity.scala
+++ src/com/example/jukugoquiz/MainActivity.scala
@@ -7,15 +7,12 @@ import android.view.View
 import android.widget._
 import java.io.IOException
 import java.util.Date
-import scala.util.Random
 
 class MainActivity extends Activity {
-  var wordList: Array[Word] = Array()
-
   override protected def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    this.initialize
+    this.showQuestion
   }
 
   override def onCreateOptionsMenu(menu: Menu): Boolean = {
@@ -46,63 +43,17 @@ class MainActivity extends Activity {
   def buttonNextClick(v: View) = showQuestion()
 
   /**
-   * case class Watch
-   */
-  case class Watch (label: String) {
-    private val start: Date = new Date
-    def log() = {
-      android.util.Log.d(label, "Watch -- " + label + " : " + ((new Date).getTime - start.getTime).toString + " ms")
-    }
-  }
-
-  /**
-   * initialize
-   */
-  private def initialize() {
-    try {
-      val watch = Watch("loadData")
-      loadData()
-      watch.log
-    } catch { case e: IOException =>
-      showMessage(e.getLocalizedMessage)
-    }
-
-    showQuestion()
-  }
-
-  /**
-   * loadData
-   */
-  private def loadData() {
-    try{
-      val br = new java.io.BufferedReader(
-        new java.io.InputStreamReader(getResources.getAssets.open("Jukugo2c_utf8.txt"))
-      )
-      val lines = Iterator.continually(br.readLine).takeWhile(_ != null).toArray
-      this.wordList = lines.map(_.split(",")).map(arr => new Word(arr(0), arr(1)))
-    } catch { case e: IOException =>
-      throw new IOException(e.getMessage.toString)
-    }
-  }
-
-  /**
    * showQuestion
    */
   private def showQuestion() {
     val watch = Watch("showQuestion")
-    val selectedWord = this.wordList((new Random).nextInt(this.wordList.size)).kanji
-    val selectedChar = selectedWord.charAt((new Random).nextInt(2))
-    val wordListFilterd: Array[Word] = this.wordList.filter(_.kanji.indexOf(selectedChar) != -1)
-    val preCandidate: List[Word] = wordListFilterd.filter(_.kanji.indexOf(selectedChar) == 1).toList
-    val postCandidate: List[Word] = wordListFilterd.filter(_.kanji.indexOf(selectedChar) == 0).toList
     
-    Answer.setAnswers(preCandidate, postCandidate, selectedChar)
-    Answer.getAnswers.zipWithIndex.foreach { case (w:Word , i: Int) =>
+    Answer.setNewAnswers.getAnswers.zipWithIndex.foreach { case (w:Word , i: Int) =>
       val vId = getResources.getIdentifier("textView" + (i + 1).toString, "id", getPackageName())
       findViewById(vId).asInstanceOf[TextView].setText(w.answerIndexToChar(i).toString)
     }
-
     findViewById(R.id.editTextAnswer).asInstanceOf[EditText].setText("")
+    
     watch.log
   }
 
@@ -113,6 +64,16 @@ class MainActivity extends Activity {
     val b = new android.app.AlertDialog.Builder(this)
     b.setMessage(s).setPositiveButton(android.R.string.ok, null).show()
   }
+  
+  case class Word(kanji: String, yomi: String) {
+    lazy val char1 = kanji.charAt(0);
+    lazy val char2 = kanji.charAt(1);
+    def answerIndexToChar(i: Int): Char = i match {
+      case i:Int if i >= 0 && i <= 3 => this.char1
+      case i:Int if i >= 4 && i <= 8 => this.char2
+      case _ => 0
+    }
+  }//end of case class Word
 
   object Answer {
     import scala.collection.mutable.ArrayBuffer
@@ -120,30 +81,41 @@ class MainActivity extends Activity {
     val answersPost = ArrayBuffer[Word]()
     private var answerChar: Char = 0
 
-    def setAnswers(preCandidate: List[Word], postCandidate: List[Word], AnswerChar: Char){
-      this.initialize
+    def setNewAnswers() = {
+      import scala.util.Random
+      
+      val selectedChar = Dictionary.words((new Random).nextInt(Dictionary.words.size)).kanji.charAt((new Random).nextInt(2))
+      val wordsFilterd = Dictionary.words.filter(_.kanji.indexOf(selectedChar) != -1)
+      val preCandidate  = wordsFilterd.filter(_.kanji.indexOf(selectedChar) == 1)
+      val postCandidate = wordsFilterd.filter(_.kanji.indexOf(selectedChar) == 0)
+      //answersPre・answersPostを初期化
+      List(answersPre, answersPost).foreach(_.clear)
+      List(answersPre, answersPost).foreach(_ ++= ArrayBuffer.fill(4)(Word("●●", "")))
       //preCandidate・postCandidateからランダムに4件づつ取り出し
-      Random.shuffle(preCandidate).take(4).zipWithIndex.foreach{ case (w:Word , i: Int) => answersPre(i) = w }
-      Random.shuffle(postCandidate).take(4).zipWithIndex.foreach{ case (w:Word , i: Int) => answersPost(i) = w }
-      this.answerChar = AnswerChar
+      Random.shuffle(preCandidate.toList).take(4).zipWithIndex.foreach{ case (w:Word , i: Int) => answersPre(i) = w }
+      Random.shuffle(postCandidate.toList).take(4).zipWithIndex.foreach{ case (w:Word , i: Int) => answersPost(i) = w }
+      this.answerChar = selectedChar
+      this
     }
     
-    def getAnswerChar = this.answerChar
-    
-    def getAnswers() = {
-      (answersPre ++ answersPost).toArray
-    }
-    
-    def getAnswersString() = {
-      (answersPre ++ answersPost).filter(_.yomi.nonEmpty).map(a => a.kanji + "  (" + a.yomi + ")").mkString("\n")
-    }
-    
-    private def initialize {
-      List(answersPre, answersPost).foreach { answers =>
-        answers.clear
-        answers ++= ArrayBuffer.fill(4)(Word("●●", ""))
-      }
-    }
+    def getAnswerChar() = this.answerChar
+    def getAnswers() = (answersPre ++ answersPost).toArray
+    def getAnswersString() = (answersPre ++ answersPost).filter(_.yomi.nonEmpty).map(a => a.kanji + "  (" + a.yomi + ")").mkString("\n")
   }// end object Answer
   
+  object Dictionary {
+    lazy val words = try{
+      val br = new java.io.BufferedReader(
+        new java.io.InputStreamReader(getResources.getAssets.open("Jukugo2c_utf8.txt"))
+      )
+      Iterator.continually(br.readLine).takeWhile(_ != null).toArray.map(_.split(",")).map(arr => new Word(arr(0), arr(1)))
+    } catch { case e: IOException => throw new IOException(e.getMessage.toString) }  
+  }//end object Dictionary
+  
+  private case class Watch (label: String) {
+    private val start: Date = new Date
+    def log() = {
+      android.util.Log.d(label, "Watch -- " + label + " : " + ((new Date).getTime - start.getTime).toString + " ms")
+    }
+  }// end case class Watch
 }
